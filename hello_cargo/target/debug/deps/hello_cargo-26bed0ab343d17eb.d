48db24cce033bd8a9c56fa6ec2837b344e5cf13f
kmsu <ingkevins@gmail.com>
2024-09-09 21:39:30 -0600

    eliminacion de archivos gitignore

--- hello_cargo/target/debug/deps/hello_cargo-26bed0ab343d17eb.d
+++
@@ -1,5 +0,0 @@
-/home/kevin/Documentos/2S2024/Sopes1/Lab/SO1_2S2024_200915348/hello_cargo/target/debug/deps/libhello_cargo-26bed0ab343d17eb.rmeta: src/main.rs
-
-/home/kevin/Documentos/2S2024/Sopes1/Lab/SO1_2S2024_200915348/hello_cargo/target/debug/deps/hello_cargo-26bed0ab343d17eb.d: src/main.rs
-
-src/main.rs:
--- hello_cargo/target/debug/hello_cargo.d
+++
@@ -1,1 +0,0 @@
-/home/kevin/Documentos/2S2024/Sopes1/Lab/SO1_2S2024_200915348/hello_cargo/target/debug/hello_cargo: /home/kevin/Documentos/2S2024/Sopes1/Lab/SO1_2S2024_200915348/hello_cargo/src/main.rs
